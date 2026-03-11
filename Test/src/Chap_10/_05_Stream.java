5a9823c8c66d2431bc6e1b44ec996afda53c47d3
hjc13 <hjc13@218.155.9.130>
2026-03-11 23:44:23 +0900

    260311_23:44

--- Test/src/Chap_10/_05_Stream.java
+++ Test/src/Chap_10/_05_Stream.java
@@ -6,12 +6,13 @@ import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+
 public class _05_Stream {
 	public static void main(String[] args) {
-		// 스트림
+		// 스트림 생성
 		
 		// Arrays.stream(); 이용
-		int[] scores = {100, 95, 85, 80};
+		int[] scores = {100, 95, 90, 85, 80};
 		IntStream scorestream = Arrays.stream(scores);
 		
 		String[] langs = {"python", "Java", "javascript", "c", "c++", "c#"};
@@ -23,11 +24,37 @@ public class _05_Stream {
 //		langList.add("Java");
 		// asList를 통해 한번에 값을 넣을 수 있다.
 		langList = Arrays.asList("python", "Java", "javascript", "c", "c++", "c#"); 
-		System.out.println(langList.size());
+//		System.out.println(langList.size());
 		Stream<String> langListStream = langList.stream();
 		
 		// Stream.of(); 이용
 		Stream<String> langListOfStream = Stream.of("python", "Java", "javascript", "c", "c++", "c#");
+		
+		
+		// 스트림 사용
+		// 중간 연산 (Intermediate Operation) : filter, map, sorted, distinct, skip,...
+		// 최종 연산 (Terminal Operation) : count, min, max, sum, forEach, antMatch, allMatch, ...
+		
+		// 90 점 이상인 점수만 출력
+		Arrays.stream(scores).filter(x -> x >= 90).forEach(x -> System.out.println(x));
+//		Arrays.stream(scores).filter(x -> x >= 90).forEach(System.out::println); // 똑같다
+		 
+		System.out.println("----------------------------");
+		
+		// 90 점 이상인 사람의 수
+		int count = (int)Arrays.stream(scores).filter(x -> x >= 90).count();
+		System.out.println(count);
+		
+		System.out.println("----------------------------");
+		
+		// 90 점 이상인 점수들의 함
+		int sum = Arrays.stream(scores).filter(x -> x >= 90).sum();
+		System.out.println(sum);
+		
+		System.out.println("----------------------------");
+		
+		// 90 점 이상인 점수들의 정렬
+		Arrays.stream(scores).filter(x -> x >= 90).sorted().forEach(System.out::println);
 		
 	}
 
---
+++ Test/src/Chap_11/_01_TryCatch.java
@@ -0,0 +1,8 @@
+package Chap_11;
+
+public class _01_TryCatch {
+	public static void main(String[] args) {
+		// 예외 처리
+	}
+
+}
