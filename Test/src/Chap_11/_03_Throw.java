278e9a6db8dd2f4426a064a968a0353718782960
sai0734 <hjc135@gmail.com>
2026-03-12 19:23:21 +0900

    260312_19:23

--- Test/src/Chap_11/_03_Throw.java
+++ Test/src/Chap_11/_03_Throw.java
@@ -7,15 +7,13 @@ public class _03_Throw {
 		
 		try {
 			if (age <= 19) {
-				//System.out.println("만 19세 미만에게는 판매하지 않습니다.");
+//				System.out.println("만 19세 미만에게는 판매하지 않습니다.");
 				throw new Exception("만 19세 미만에게는 판매하지 않습니다.");
 			} else {
 				System.out.println("주문하신 상품 여기 있습니다.");
-			}
-			
+			}			
 		} catch (Exception e) {
-			e.printStackTrace();
-			
+			e.printStackTrace();	
 		}
 	}
 
