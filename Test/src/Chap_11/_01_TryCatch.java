253f6ea84dcfadadb505d4a4940ad94f7aba74d7
sai0734 <hjc135@gmail.com>
2026-03-12 17:24:15 +0900

    260312_17:24

--- Test/src/Chap_11/_01_TryCatch.java
+++ Test/src/Chap_11/_01_TryCatch.java
@@ -3,6 +3,38 @@ package Chap_11;
 public class _01_TryCatch {
 	public static void main(String[] args) {
 		// 예외 처리
+		// 에러 error, 예뢰 exception
+		// 컴파일 오류 : 코드에 문제가 있어서 프로그램이 실행조차 안됨
+		// int i = "문자열";
+		// 런타임 오류 : 컴파일은 되지만 실행하는 도중 오류가 발생
+		// int[] i = new int[3];
+		// int[5] = 0;
+		
+//		S s = new S();
+//		s.methodA(); // StackOverflowError 에러 발생
+		
+//		S a = null;
+//		a.methodA(); // NullPointerException 에러 발생
+		
+		try { 
+//			System.out.println(3/ 0);
+//			int[] arr = new int[3];
+//			arr[5] = 100; 
+			Object obj = "test";
+			 System.out.println((int) obj);
+			
+		} catch (Exception e) {
+			System.out.println("이런 문제가 발생했어요ㅕ => " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+	
+		
+		
+		
+		
+		
+		System.out.println("프로그램 정상 종료");
 	}
 
 }
---
+++ Test/src/Chap_11/_07_Throws.java
@@ -0,0 +1,33 @@
+package Chap_11;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class _07_Throws {
+	public static void main(String[] args) {
+		try {
+			weiteFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("메인 메소드에서 해결할게요.");
+		}
+		
+		
+	}
+	
+	public static void weiteFile() throws IOException {
+//		try {
+//			FileWriter writer = new FileWriter("test.txt");
+//			throw new IOException("파일 쓰기에 실패했어요!!");
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			System.out.println("메소드 내에서 자체 해결했어요.");
+//		}
+		
+		FileWriter writer = new FileWriter("test.txt");
+		throw new IOException("파일 쓰기에 실패했어요!!");
+		
+	}
+
+}
